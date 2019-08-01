b1b58b509d378019c3293b6f1b7c5455a0a90041
kari <mr_hm@outlook.com>
2019-08-02 01:33:25 +0800

    完成 button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './Button';
 import Icon from './Icon';
+import ButtonGroup from './button-group'
 
 Vue.component('w-button', Button)
 Vue.component('w-icon', Icon)
+Vue.component('button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="button-group">
+    <slot></slot>
+</div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .button-group {
+        display: inline-flex; vertical-align: middle;
+        > .w-button {border-radius: 0; margin-left: -1px;
+            &:hover {position: relative; z-index: 1;}
+            &:first-child {border-bottom-left-radius: var(--border-radius);
+                border-top-left-radius: var(--border-radius); margin-left: 0;}
+            &:last-child {border-bottom-right-radius: var(--border-radius);
+                border-top-right-radius: var(--border-radius);}
+        }
+    }
+</style>
