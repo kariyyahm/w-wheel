6fb5811da0026a1b85acb17b84f4e03264e93d1c
kari <mr_hm@outlook.com>
2019-08-06 22:13:47 +0800

    完成 collapse

--- src/app.js
+++ src/app.js
@@ -18,7 +18,11 @@ import TabsHeadItem from './tabs-item'
 import TabsBody from './tabs-body'
 import TabsBodyPane from './tabs-pane'
 import Popover from './Popover'
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
 
+Vue.component('w-collapse', Collapse)
+Vue.component('w-collapse-item', CollapseItem)
 Vue.component('w-popover', Popover)
 Vue.component('w-toast', Toast)
 Vue.component('w-button', Button)
@@ -47,7 +51,8 @@ new Vue({
         loading2: true,
         loading3: false,
         message: '',
-        selectedTab: 'caijing'
+        // selectedTab: 'caijing',
+        selectedTab: ['2']
     },
     created() {
 
@@ -98,110 +103,3 @@ new Vue({
         }
     }
 })
-
-// 单元测试
-
-import chai from 'chai'
-import spies from 'chai-spies'
-
-chai.use(spies)
-const expect = chai.expect
-
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-settings')
-
-    // 删除button
-    button.$el.remove()
-    button.$destroy()
-}
-// 测试按钮含有loaidng
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#icon-loading')
-
-
-    // 删除button
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-{
-    // 生成一个div来挂载button里的svg，order才会被渲染
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-
-    // 删除button
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-
-{
-    // 生成一个div来挂载button里的svg，order才会被渲染
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            iconPosition: 'right'
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-
-    // 删除button
-    vm.$el.remove()
-    vm.$destroy()
-}
-
-
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    vm.$mount()
-    let spyA = chai.spy(function () {
-    })
-
-    vm.$on('click', spyA)
-    let button = vm.$el
-    button.click()
-    expect(spyA).to.have.been.called()
-}
-
